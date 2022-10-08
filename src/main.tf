c63d67eaf20b4cc3a6b6c6810cf730b8efcb483d
CumulusCycles <cumuluscycles@gmail.com>
2022-10-08 08:28:21 -0400

    init commit

---
+++ src/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
+  # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
+  backend "s3" {
+    bucket         = "cc-tf-state-backend-ci-cd"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "tf-state" {
+  source      = "./modules/tf-state"
+  bucket_name = "cc-tf-state-backend-ci-cd"
+}
